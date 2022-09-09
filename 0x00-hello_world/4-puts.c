07cd63d60f2a56be7de720b48efcd414985ce4da
Macaulay-Ayomide <ayomideizzi@gmail.com>
2022-09-08 22:21:22 -0700

    please meet up

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,5 @@
 int main(void) 
 /**first program in c*/
 {
-printf("\"Programming is like building a multilingual puzzle\n");
-return 0;
+puts("\"Programming is like building a multilingual puzzle\n");
 } 
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,5 +3,4 @@ int main(void)
 /**first program in c*/
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
 } 
