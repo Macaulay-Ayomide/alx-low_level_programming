5397a94d7d169b25827f7f1d421fa0e3e7f60242
Macaulay-Ayomide <ayomideizzi@gmail.com>
2022-09-09 02:22:13 -0700

    push betty fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main(void) 
-/**first program in c*/
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return 0;
-} 
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
