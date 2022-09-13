12a4ea8017c9b4c745e50afab7dc33fdef32a482
Macaulay-Ayomide <ayomideizzi@gmail.com>
2022-09-12 21:40:20 -0700

    one more task to go

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main(void) 
-/**first program in c*/
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return 1;
-} 
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
