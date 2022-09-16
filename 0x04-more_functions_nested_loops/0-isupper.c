966fc0f2a9052607e903ffe744841bd899b24f05
Macaulay-Ayomide <ayomideizzi@gmail.com>
2022-09-16 15:54:20 -0700

    lets see

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Description: 'this is a file comtaining a function called _isupper()'
- * @c >>> a inputed parameter caontaing a character
- * Return: Always 0 (Success)
+ *_isupper- Check if a letter is an uppercase
+ * @c: int input less than 128
+ * Description: Takes an int and checks if its within the range of uppercase
+ * letters from 65 to  91
+ * Return: The value returned is 0
  */
 
 int _isupper(int c)
