d0c78ffedfb32fb640453d74009a76b456e1cc60
Macaulay-Ayomide <ayomideizzi@gmail.com>
2022-09-23 22:59:11 -0700

    come-on

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strcat - Copying from source to destination
+ * @dest: takes a pointer as an argument
+ * @src: takes a pointer as an argument
+ * Description: ..........................
+ * Return: The value returned is 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i,j;
+	
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; j < n + 1 && src[j] == '\0' ; j++)
+	{
+		dest[i++] = src[j];
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+int _isdigit(int c);
+void puts_half(char *str);
+void print_array(int *a, int n);
+void print_diagonal(int);
+int _putchar(char c);
