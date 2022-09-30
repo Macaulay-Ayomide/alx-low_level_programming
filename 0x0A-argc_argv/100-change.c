f642418c3fa8b420703a825ea4b3bf366035fa6f
Macaulay-Ayomide <ayomideizzi@gmail.com>
2022-09-30 14:37:24 -0700

    cc

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,6 +12,20 @@
 
 int main(int argc, char *argv[])
 {
+	int count, num;
+
+	count = 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	num = atoi(argv[1]);
+	if (num <= 0)
+	{
+		printf("%d", 0);
+		return (0);
+	}
 	while (num > 0)
 	{
 		if (num >= 25)
